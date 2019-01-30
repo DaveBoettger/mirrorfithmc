1e7ea6d53e96e53a4efec2a44d766b8652739568
David Boettger <daveboettger@gmail.com>
2019-01-30 18:59:11 -0300

    Revisions to the dataset class to enable rapid creation of more flexible model

--- mirrorfit.py
+++ mirrorfit.py
@@ -1,4 +1,8 @@
 import numpy as np
+import theano.tensor as tt
+from collections import namedtuple
+
+DatasetArrays = namedtuple('DatasetArrays', ['pos', 'err'])
 
 class point(object):
     def __init__(self):
@@ -35,71 +39,122 @@ class point(object):
         p.type = self.type
         return p
 
-class dataset(list):
+class dataset(dict):
 
-    def __init__(self, points=None, from_file=None):
+    def __init__(self, points=None, from_file=None, markers=None):
+
         super().__init__()
-        #self.trnsf = transform_history()
+
+        if markers is not None:
+            self.markers = markers
+        else:
+            self.markers = []
+
         if points is not None:
-            if isinstance(points[0],point):
-                self.extend(points)
-            else:
-                for x in points:
-                    p = point()
-                    p.pos=x
-                    p.type = "OTHER"
-                    self.append(p)
+            for point in points:
+                self.add_point(point)
         if from_file is not None:
             self.read_data_file(from_file)
-        self._setup()
 
     def copy(self):
         c = dataset()
-        for p in self: c.add_point(p.copy())
+        for p in self.values(): c.add_point(p.copy())
         return c
         
     def add_point(self,point):
-        self.append(point)
-        self._setup()
+        self[point.label] = point
 
     def remove_point(self, point):
-        self.remove(point)
-        self._setup()
-        
-    def _setup(self):
-        self.npoint = len(self)
-        self.code = self._gettype("CODE")
-        self.target = self._gettype("TARGET")
-        self.nugget = self._gettype("NUGGET")
-        self.csb = self._gettype("CSB")
-        self.other = self._gettype("OTHER")
-        self.ncode = np.sum(self.code)
-        self.ntarget = np.sum(self.target)
-        self.nnugget = np.sum(self.nugget)
-        self.ncsb = np.sum(self.csb)
-        self.nother = np.sum(self.other)
+        del self[point.label]
 
     def read_data_file(self, filename):
         with open(filename) as f:
             for l in f:
-                if l[0]!="#":
+                if l[0] == '*':
+                    self.markers.append(l[1:].strip().upper())
+                elif l[0]!="#":
                     p = point()
                     p.read_ph_file_line(l)
                     if p.label == "":
                         p.label = "TARGET_%d"%(self.ntarget+1)
                     self.add_point(p)
 
-    def _gettype(self, type):
-        return np.array([t.type == type for t in self])
+    def __getattr__(self, name):
+
+        if name == 'markers':
+            #ideally self.markers should always be defined as something, but if not
+            #set it as an empty list and return it. This prevents infinite recursion
+            newlist = []
+            self.__setattr__('markers', newlist)
+            return newlist
+
+        elif name == 'labels':
+            return [p.label for p in self.values()]
+        elif name == 'pos':
+            return [p.pos for p in self.values()]
+        elif name == 'error' or name == 'err':
+            return [p.err for p in self.values()]
+        elif name.upper() in self.markers:
+            return self.subset_from_marker(name.upper())
+        else:
+            raise AttributeError
 
     def getAttribute(self, att, sel = None):
+        '''Legacy function to implement retrival of some attributes while also using a selection argument.
+        Consider removing.
+        '''
+
         if sel is None: sel = range(self.npoint)
         elif type(sel[0]) == bool or type(sel[0]) == np.bool_: sel = np.where(sel)[0]
         return np.array([self[p].__dict__[att] for p in sel])
 
-    def toArray(self, sel=None):
-        return self.getAttribute("pos",sel=sel)
+    def to_array(self):
+        '''Return the positions of points in the dataset as a numpy array'''
+        return np.array(self.pos)
 
-    def toErrArray(self, sel=None):
-        return self.getAttribute("err")
+    def to_err_array(self):
+        '''Return the errors of points in the dataset as a numpy array'''
+        return np.array(self.error)
 
+    def to_tensor(self):
+        '''Return the positions of points in the dataset as a Theano tensor'''
+        arr = self.to_array()
+        return tt._shared(arr.T)
+
+    def to_err_tensor(self):
+        '''Return the positions of points in the dataset as a Theano tensor'''
+        arr = self.to_err_array()
+        return tt._shared(arr.T)
+
+    def to_arrays(self):
+        return DatasetArrays(pos=self.to_array(), err=self.to_err_array())
+
+    def to_tensors(self):
+        return DatasetArrays(pos=self.to_tensor(), err=self.to_err_tensor())
+
+    def subset_from_labels(self, labels):
+        '''Return a new dataset as a subset of self by exactly matching labels'''
+
+        return dataset(points=[self[p] for p in self if p in labels])
+
+    def subset_from_marker(self, marker):
+        '''Return a new dataset as a subset of self by matching a search string IN the point labels'''
+
+        return dataset(points=[self[p] for p in self if marker in p])
+
+    def labels_in_common(self, other, marker=None):
+        '''Return a list of labels with labels in common between this dataset and another,
+        optionally using marker to further shrink the subset'''
+
+        if marker is None:
+            return [l for l in self if l in other and l]
+        else:
+            print(marker)
+            return [l for l in self if l in other and marker in l]
+
+    def subsets_in_common(self, other, marker=None):
+        '''Return a subset with labels in common between this dataset and another,
+        optionally using marker to further shrink the subset'''
+
+        common_labels = self.labels_in_common(other, marker)
+        return (self.subset_from_labels(common_labels), other.subset_from_labels(common_labels))
--- testmf.py
+++ testmf.py
@@ -4,10 +4,8 @@ import matplotlib.pyplot as plt
 import theano.tensor as tt
 import theano
 import mirrorfit as mf
-from collections import namedtuple
+import json
 import time
-
-PrimaryTmap = namedtuple('PrimaryTMAP', 'tx, ty, tz, rx, ry, rz, s, R')
 
 def dist(val1, val2):
 
@@ -55,82 +53,94 @@ def theano_rot(rx,ry,rz, rescale=True):
     full_rotation=tt.dot(Rzt,tt.dot(Ryt, Rxt))
     return full_rotation
 
-def dataset_to_tensors(dataset):
-    arr = dataset.toArray()
-    errs = dataset.toErrArray()
 
-    pos_tensor = tt._shared(arr.T)
-    err_tensor = tt._shared(errs.T)
-    return pos_tensor, err_tensor
+def apply_trans():
 
-def getRSqFun(r_off=0., angle_off=0.):
-    '''Returns the radius to the origin of the symmetrized coordinate system given x,y in non-symmetrized coordinates'''
-    def r(x,y):
-        return (x-r_off*np.cos(angle_off))**2+(y-r_off*np.sin(angle_off))**2
-    
-    return r
+    new_pos = (s/100.)*tt.dot(rot,pos)+(1./1000.)*t[:,np.newaxis]
+    new_err = abs((s/100.)*tt.dot(rot,err))
 
-def get_model(pos, err, TMAP=None):
+def load_param_file(fName):
+
+    if fName.endswith('.json'):
+        with open(fName) as f:
+            return json.load(f)
+
+class Mirror(pm.Model):
+
+    def __init__(self, definition_file, name='', model=None):
+        self.definition = load_param_file(definition_file)
+        try:
+            if name == '':
+                name = self.definition['default_name']
+        except KeyError:
+            pass
+        super(Mirror, self).__init__(name, model)
+        self.R = self.definition['geometry']['R']
+        self.k = self.definition['geometry']['k']
+
+def get_primary_model(pos, err, TMAP=None, model=None):
 
     if TMAP is None:
         TMAP = PrimaryTmap(1.,1.,1.,1.,1.,1.,0.,1.)
     print(TMAP)
-    with pm.Model() as model:
+    if model is None:
+        model = pm.Model()
+    with model:
 
         #Weak priors on translations and rotations. 
 
         #Translation variables:
-        if TMAP.tx:
-            tx = pm.Normal('tx', mu=0, sd=1000.)
+        if TMAP['tx']:
+            prim_tx = pm.Normal('tx', mu=0, sd=1000.)
         else:
-            tx = 0.
-        if TMAP.ty:
-            ty = pm.Normal('ty', mu=0, sd=1000.)
+            prim_tx = 0.
+        if TMAP['ty']:
+            prim_ty = pm.Normal('ty', mu=0, sd=1000.)
         else:
-            ty = 0.
-        if TMAP.tz:
-            tz = pm.Normal('tz', mu=0, sd=1000.)
+            prim_ty = 0.
+        if TMAP['tz']:
+            prim_tz = pm.Normal('tz', mu=0, sd=1000.)
         else:
-            tz = 0.
+            prim_tz = 0.
         #Rotation variables:
-        if TMAP.rx:
-            rx = pm.Uniform('rx', lower=-20., upper=20.)
+        if TMAP['rx']:
+            prim_rx = pm.Uniform('rx', lower=-20., upper=20.)
         else:
-            rx = 0.
-        if TMAP.ry:
-            ry = pm.Uniform('ry', lower=-20., upper=20.)
+            prim_rx = 0.
+        if TMAP['ry']:
+            prim_ry = pm.Uniform('ry', lower=-20., upper=20.)
         else:
-            ry = 0.
-        #rz = pm.Uniform('rz', lower=0, upper=2*np.pi)
-        if TMAP.rz:
-            rz = pm.Uniform('rz', lower=-20., upper=20.)
+            prim_ry = 0.
+        if TMAP['rz']:
+            prim_rz = pm.Uniform('rz', lower=-20., upper=20.)
         else:
-            rz=0.
-        if TMAP.s:
-            s = pm.Normal('s', mu=100, sd=10.)
+            prim_rz=0.
+        #Scale
+        if TMAP['s']:
+            prim_s = pm.Normal('s', mu=100, sd=10.)
         else:
-            s=100.
+            prim_s=100.
+        #R - behaves a lot like scaling the mirror instead of scaling the dataset
+        if TMAP['R']:
+            prim_R = pm.Normal('R', mu=4400., sd=5)
+        else:
+            prim_R = 4400.
 
-        if TMAP.R:
-            R = pm.Normal('R', mu=4400., sd=5)
-        else:
-            R = 4400.
-
-        std = pm.Normal('std_intrinsic', mu=40., sd=30.)
+        prim_std = pm.Normal('std_intrinsic', mu=40., sd=30.)
         c = 1./R
         k = -1.
         target_thickness = .2
-        rot = theano_rot(rx,ry,rz)
-        roffset = theano.shared(np.array([0,2800.,0.]))
-        t = tt.stacklists([tx,ty,tz])
-        new_pos = (s/100.)*tt.dot(rot,pos)+(1./1000.)*t[:,np.newaxis]
-        new_err = abs((s/100.)*tt.dot(rot,err))
-        rsq = new_pos[0]**2+new_pos[1]**2
-        sigmarsq = (new_err[0]**2*new_pos[0]+new_err[1]**2*new_pos[1])/rsq
-        con = (c*rsq)/(1.+tt.sqrt(1.-(1.+k)*c**2.*rsq))
-        a = tt.sqrt((R**2. - rsq*(k + 1.))/R**2.)
-        mrsq = (tt.sqrt(rsq)*(3.*R**2.*a*(a + 1.) + rsq*(k + 1.))/(R**3.*a*(a + 1.)**2.))**2
-        e_rescale = 1./(mrsq+1.)
+        prim_rot = theano_rot(rx,ry,rz)
+        #roffset = theano.shared(np.array([0,2800.,0.]))
+        prim_t = tt.stacklists([tx,ty,tz])
+        prim_new_pos = (prim_s/100.)*tt.dot(prim_rot,pos)+(1./1000.)*prim_t[:,np.newaxis]
+        prim_new_err = abs((s/100.)*tt.dot(rot,err))
+        prim_rsq = new_pos[0]**2+new_pos[1]**2
+        prim_sigmarsq = (new_err[0]**2*new_pos[0]+new_err[1]**2*new_pos[1])/rsq
+        prim_con = (c*rsq)/(1.+tt.sqrt(1.-(1.+k)*c**2.*rsq))
+        prim_a = tt.sqrt((R**2. - rsq*(k + 1.))/R**2.)
+        prim_mrsq = (tt.sqrt(rsq)*(3.*R**2.*a*(a + 1.) + rsq*(k + 1.))/(R**3.*a*(a + 1.)**2.))**2
+        prim_e_rescale = 1./(mrsq+1.)
         dist = (((new_pos[2]-con)*tt.sqrt(e_rescale)) - target_thickness)*1000.
 
         dist_error = 1000*tt.sqrt(new_err[2]**2*e_rescale + mrsq*e_rescale*sigmarsq)
@@ -142,19 +152,28 @@ def get_model(pos, err, TMAP=None):
 
 if __name__ == '__main__':
 
-    #data = mf.dataset(from_file='20181205_primary.txt')
-    data = mf.dataset(from_file='20181208_primary.txt')
-    primary_points = [pp for pp in data if 'PRIMARY' in pp.label]
+    print(mf.__file__)
+    data = mf.dataset(from_file='20181208_primary_receiver.txt')
+    print(data.ntarget)
+
+    primary_points = data.subset_from_label('PRIMARY')
     primary = mf.dataset(points=primary_points)
-
-    len_mds = len(primary)
-    pos,err = dataset_to_tensors(primary)
-    #for r in np.arange(-np.pi/8,np.pi/8.,.1):
-    #    print_model_params(tx=10, ty=10, ry=r)
-    #quit()
-
-    TMAP = PrimaryTmap(tx=True,ty=True, tz=True, rx=True, ry=True, rz=False, s=False, R=True)
-    model = get_model(pos,err,TMAP=TMAP)
+    pos = primary.to_tensor()
+    err = primary.to_err_tensor()
+    TMAP = {'tx':True, 'ty':True, 'tz':True, 'rx':True, 'ry':True, 'rz':False, 's':False, 'R':True}
+    test = Mirror(definition_file='POLARBEAR/SA_Primary.json')
+    with pm.Model() as model:
+        print(test)
+    with test as model:
+        r=22
+    with test as model:
+        test2 = test.test()
+    with model:
+        trace = pm.sample(2000, tune=1000, init = 'advi+adapt_diag', nuts_kwargs={'target_accept': .95, 'max_treedepth': 15})
+    pm.traceplot(trace)
+    plt.show()
+    quit()
+    model = get_primary_model(pos,err,TMAP=TMAP)
     with model:
         trace = pm.sample(2000, tune=1000, init = 'advi+adapt_diag', nuts_kwargs={'target_accept': .95, 'max_treedepth': 15})
 
