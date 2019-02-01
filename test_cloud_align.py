e22a4a4864a8f04ade776a554741bf869577bf1d
David Boettger <daveboettger@gmail.com>
2019-02-01 19:01:22 -0300

    Fix typo and import error

--- test_cloud_align.py
+++ test_cloud_align.py
@@ -1,4 +1,7 @@
 import mirrorfit as mf
+import pymc3 as pm
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
@@ -6,7 +9,6 @@ if __name__ == '__main__':
     ds2 = mf.dataset(from_file='20181208_primary_receiver.txt') 
     with mf.AlignDatasetSimple(ds1=ds1,ds2=ds2) as model: 
         trace = pm.sample(2000, tune=1000, init = 'advi+adapt_diag', nuts_kwargs={'target_accept': .95, 'max_treedepth': 15}) 
-             
-     distances = trace.get_values('Alignment_distance') 
-
-     errors = trace.get_values('Alignment_errors')
+     
+    distances = trace.get_values('Alignment_distance') 
+    errors = trace.get_values('Alignment_errors')
